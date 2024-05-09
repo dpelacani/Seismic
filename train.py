eca3ac121d96924397f89a77ce51a0109fbe3ad6
dekape <deborah.pelacani@gmail.com>
2024-05-09 14:17:23 +0100

    training with accelerator

--- train.py
+++ train.py
@@ -8,6 +8,9 @@ from tqdm import tqdm
 import wandb
 from torchsummary import summary
 
+from accelerate import Accelerator
+from accelerate.utils import broadcast_object_list
+
 from seismic import build_model
 
 sys.path.append("/home/dp4018/scripts/InverseLDM/")
@@ -18,7 +21,7 @@ from invldm.utils.setup import set_seed
 
 set_seed(42)
 
-DEVICE="cuda:0"
+accelerator = Accelerator(log_with="wandb")
 
 if __name__ == "__main__":
 
@@ -47,7 +50,7 @@ if __name__ == "__main__":
 
         ),
         sampling_only=False,
-        batch_size=8,
+        batch_size=32,
     ))
 
     dataset = Brain2DDataset(dataset_args)
@@ -66,6 +69,7 @@ if __name__ == "__main__":
     vision_patch_size = 3
     mixed_precision = False
 
+    set_seed(42)
     model = build_model(
         embed_dim,
         embed_size,
@@ -77,27 +81,9 @@ if __name__ == "__main__":
         vision_width,
         vision_patch_size,
         mixed_precision
-    ).to(DEVICE)
-
-    _ = summary(model)
-
-    img_f, data_f, img_logit, data_logit = model(img.to(DEVICE), data.to(DEVICE))
-
-    print("\n================ LOGITS ==================")
-    print(img_logit.shape, data_logit.shape)
-    print(img_logit.min().item(), img_logit.max().item(), img_logit.mean().item())
-    print(data_logit.min().item(), data_logit.max().item(), data_logit.mean().item())
-
-    print("\n================ FEATURES ================")
-    print(img_f.shape, data_f.shape)
-    print(img_f.min().item(), img_f.max().item(), img_f.mean().item())
-    print(data_f.min().item(), data_f.max().item(), data_f.mean().item())
-
-    del img_f, data_f, img_logit, data_logit
+    )
 
     save_freq = 500
-
-
     n_epochs = 450
     warm_restart_steps = 2000
     bias_weight_decay = False
@@ -107,15 +93,35 @@ if __name__ == "__main__":
     scheduler = None
     losses = []
 
+    # if accelerator.is_main_process:
+    #     _ = summary(model)
+
+    #     img_f, data_f, img_logit, data_logit = model(img, data)
+    #     # img_f, data_f, img_logit, data_logit = model(img.to(DEVICE), data.to(DEVICE))
+
+    #     print("\n================ LOGITS ==================")
+    #     print(img_logit.shape, data_logit.shape)
+    #     print(img_logit.min().item(), img_logit.max().item(), img_logit.mean().item())
+    #     print(data_logit.min().item(), data_logit.max().item(), data_logit.mean().item())
+
+    #     print("\n================ FEATURES ================")
+    #     print(img_f.shape, data_f.shape)
+    #     print(img_f.min().item(), img_f.max().item(), img_f.mean().item())
+    #     print(data_f.min().item(), data_f.max().item(), data_f.mean().item(), "\n\n")
+
+    #     del img_f, data_f, img_logit, data_logit
+
     # Initialise Weights and Biases and store hyperparameters
-    wandb_run = wandb.init(
-        project="SeismicClip",
-        save_code=True,
-        group="hyper_tune",
-        dir="./exps",
+    accelerator.init_trackers(
+        project_name="SeismicClip",
+        init_kwargs=dict(wandb=dict(
+            save_code=True,
+            group="hyper_tune",
+            dir="./exps",
+        )),
         config={
-            "data":namespace2dict(dataset_args),
-            "training":{
+            "data": namespace2dict(dataset_args),
+            "training": {
                 "epochs": n_epochs,
             },
             "model": {
@@ -132,7 +138,7 @@ if __name__ == "__main__":
                 # "comment": "original clip parameters (some scaling by 4 given the data dimension)",
                 "loss_fn": str(loss_fn)
             },
-            "optim":{
+            "optim": {
                 "optimiser": {"name":optimiser.__class__.__name__,
                             "betas": optimiser.defaults["betas"],
                             "weight_decay": optimiser.defaults["weight_decay"],
@@ -145,12 +151,17 @@ if __name__ == "__main__":
         },
     )
 
-    # Create exp folder and save config
-    os.mkdir(f"./exps/{wandb_run.name}")
-    os.mkdir(f"./exps/{wandb_run.name}/embeddings")
-    with open(os.path.join(f"./exps/{wandb_run.name}", "config_training.yml"), "w") as f:
-        yaml.dump(dict(wandb_run.config.items()), f, default_flow_style=False, sort_keys=False, indent=4)
-
+    # Get tracker, create folders
+    wandb_run = accelerator.get_tracker("wandb", unwrap=True)
+    wandb_run_name = None
+    if accelerator.is_main_process:
+        # wandb_run_name = broadcast_object_list([wandb_run.name])[0]
+        wandb_run_name = wandb_run.name
+        # Create exp folder and save config
+        os.mkdir(f"./exps/{wandb_run_name}")
+        os.mkdir(f"./exps/{wandb_run_name}/embeddings")
+        with open(os.path.join(f"./exps/{wandb_run_name}", "config_training.yml"), "w") as f:
+            yaml.dump(dict(wandb_run.config.items()), f, default_flow_style=False, sort_keys=False, indent=4)
 
     # Change weight decay of biases to zero if prompted
     if not bias_weight_decay and optimiser.defaults["weight_decay"] > 0:
@@ -160,82 +171,121 @@ if __name__ == "__main__":
                 continue           
             elif len(param.shape) == 1 or name.endswith(".bias"):
                 no_decay_params.append(param)
-            else: decay_params.append(param)
+            else:
+                decay_params.append(param)
         params = [{'params': no_decay_params, 'weight_decay': 0.}, {'params': decay_params, 'weight_decay': optimiser.defaults["weight_decay"]}]        
     else:
         params=model.parameters()
     optimiser = torch.optim.Adam(params, **optimiser.defaults)
-
     # Step count
-    step = 0
+    global_step = torch.tensor([0])
+    step = torch.tensor([0]).to(accelerator.device)
+
+    # Accelerator wrapers
+    model, optimiser, dataloader, scheduler = accelerator.prepare(model, optimiser, dataloader, scheduler)
+    
+    # Broadcast name for saving
+    wandb_run_name = broadcast_object_list([wandb_run_name])[0]
+
 
     # Train
     try:
         for epoch in tqdm(range(n_epochs)):
             for img, data in dataloader:
-                with torch.autocast(device_type="cuda" if "cuda" in DEVICE else "cpu"):
+
+                # Update global step
+                global_step = accelerator.reduce(step).item() + accelerator.process_index 
+
+                # Evaluate model and loss at mixed precision
+                with torch.autocast(device_type="cuda" if "cuda" in str(accelerator.device) else "cpu"):
                     # Get logits
-                    _, _, logit_im, logit_dt = model(img.to(DEVICE), data.to(DEVICE))
+                    _, _, logit_im, logit_dt = model(img, data)
 
                     # Compute  loss
-                    targets = torch.arange(img.shape[0]).to(DEVICE)
+                    targets = torch.arange(img.shape[0]).to(accelerator.device)
                     loss = 0.5*(loss_fn(logit_im, targets) + loss_fn(logit_dt, targets))
                     
                 # Store loss
                 losses.append(loss.item())
 
                 # Log wandb
-                wandb.log({
+                accelerator.log({
                     "train/train_loss": loss,
                     "train/epoch": epoch,
                 })
-                if step % save_freq == 0 or step == n_epochs*len(dataloader)-1:
-                    w_logit_im = wandb.Image(logit_im, caption=f"Logits of Images - step {step}")
-                    w_logit_dt = wandb.Image(logit_dt, caption=f"Logits of Data - step {step}")
-                    wandb.log({
+
+                if global_step % save_freq == 0 or global_step == n_epochs*len(dataloader) - 1:
+                    w_logit_im = wandb.Image(logit_im, caption=f"Logits of Images - step {global_step}")
+                    w_logit_dt = wandb.Image(logit_dt, caption=f"Logits of Data - step {global_step}")
+                    accelerator.log({
                         "train/logit_images": w_logit_im,
                         "train/logit_data": w_logit_dt,
                     })
 
-                    torch.save({
-                        "model": model,
+                    accelerator.save({
+                        "model": accelerator.unwrap_model(model),
                         "epoch": epoch,
                         "step": step,
                         "optimiser": optimiser,
                         "dataset": dataset,
                         "lr_scheduler": scheduler,
                         "losses": losses,
-                    }, f"./exps/{wandb_run.name}/{wandb_run.name}-ckpt.pt")
+                    }, f"./exps/{wandb_run_name}/{wandb_run_name}-ckpt.pt")
 
                 # Zero grad and back propagation
                 optimiser.zero_grad()
-                loss.backward()
-
+                accelerator.backward(loss)
 
                 # Update gradients and scheduler
                 optimiser.step()
-                # scheduler.step()
+                if scheduler is not None:
+                    scheduler.step()
 
                 # Clip logit scaler
                 with torch.no_grad():
-                    model.logit_scale.clamp_(max=np.log(100))
+                    model.module.logit_scale.clamp_(max=np.log(100))
 
                 # Increment step Count
-                step+= 1
+                step += 1
+
     except KeyboardInterrupt:
-        wandb.finish()
-    wandb.finish()
+        accelerator.save({
+            "model": accelerator.unwrap_model(model),
+            "epoch": epoch,
+            "step": step,
+            "optimiser": optimiser,
+            "dataset": dataset,
+            "lr_scheduler": scheduler,
+            "losses": losses,
+        }, f"./exps/{wandb_run_name}/{wandb_run_name}-ckpt.pt")
+        accelerator.end_training()
+        raise KeyboardInterrupt
 
-    # Evaluate dataset
-    N = len(dataset)
-    for i in range(N):
-        d_path = dataset.data_paths[i]
-        d_name = d_path.split("/")[-1].split(".")[0] 
-        img, data = dataset[i]
-        
-        image_features = model.encode_image(img.unsqueeze(0).to(next(iter(model.parameters())).device)).detach().cpu().numpy()
-        data_features = model.encode_data(data.unsqueeze(0).to(next(iter(model.parameters())).device)).detach().cpu().numpy()
+    accelerator.save({
+        "model": accelerator.unwrap_model(model),
+        "epoch": epoch,
+        "step": step,
+        "optimiser": optimiser,
+        "dataset": dataset,
+        "lr_scheduler": scheduler,
+        "losses": losses,
+    }, f"./exps/{wandb_run_name}/{wandb_run_name}-ckpt.pt")
+    accelerator.end_training()
 
-        np.save(f"./exps/{wandb_run.name}/embeddings/{d_name}-sclip-data-features.npy", data_features[0])
-        np.save(f"./exps/{wandb_run.name}/embeddings/{d_name}-sclip-image-features.npy", image_features[0])
+    accelerator.wait_for_everyone()
+    if accelerator.is_main_process:
+        model = accelerator.unwrap_model(model)
+
+        # Evaluate dataset
+        N = len(dataset)
+        for i in range(N):
+            d_path = dataset.data_paths[i]
+            d_name = d_path.split("/")[-1].split(".")[0] 
+            img, data = dataset[i]
+            
+            image_features = model.encode_image(img.unsqueeze(0).to(next(iter(model.parameters())).device)).detach().cpu().numpy()
+            data_features = model.encode_data(data.unsqueeze(0).to(next(iter(model.parameters())).device)).detach().cpu().numpy()
+
+            np.save(f"./exps/{wandb_run_name}/embeddings/{d_name}-sclip-data-features.npy", data_features[0])
+            np.save(f"./exps/{wandb_run_name}/embeddings/{d_name}-sclip-image-features.npy", image_features[0])
 
